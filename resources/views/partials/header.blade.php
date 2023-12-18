dea025f238995692a8fc9b3d67c1af78d65bbabf
federicomanni97 <manni_federico@libero.it>
2023-12-18 15:09:39 +0100

    header with logo added

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,19 @@
-<div>Header <i class="fa fa-home"></i></div>
+<div class="container">
+    <div class="d-flex justify-content-between align-items-center p-4">
+        <img src="{{Vite::asset('/resources/img/dc-logo.png')}}" alt="">
+        <div class="text-uppercase">
+            <ul class="d-flex list-unstyled">
+                <li class="px-4"><a href="">Characters</a></li>
+                <li class="px-4"><a href="">Comics</a></li>
+                <li class="px-4"><a href="">Movies</a></li>
+                <li class="px-4"><a href="">Tv</a></li>
+                <li class="px-4"><a href="">Games</a></li>
+                <li class="px-4"><a href="">Collectibles</a></li>
+                <li class="px-4"><a href="">Videos</a></li>
+                <li class="px-4"><a href="">Fans</a></li>
+                <li class="px-4"><a href="">News</a></li>
+                <li class="px-4"><a href="">Shop</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
