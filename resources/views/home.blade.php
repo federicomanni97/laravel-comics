3de3293400eb27ed82a2411995d206108cdce02d
federicomanni97 <manni_federico@libero.it>
2023-12-18 15:39:13 +0100

    cards printed with route on home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,17 @@
 
 <main>
     <h1>Home Page</h1>
+    <div class="container">
+        <div class="row">
+            @foreach ($products as $product)
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img src="{{$product['thumb']}}" alt="{{$product['title']}}">
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </main>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $products = config('db.comics');
+    // dd($products);
+    return view('home', compact('products'));
 });
