1fa5157d64457d38dfb70b4063671f7d3a739823
federicomanni97 <manni_federico@libero.it>
2023-12-18 17:10:50 +0100

    top-footer added

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,51 @@
-<div>Footer</div>
+<footer class="top-footer">
+        <div class="container right-logo d-flex py-3 text-light position-">
+            <div class="flex-column">
+                <div class="px-3">
+                    <h5>DC COMICS</h5>
+                    <ul class="p-0 ">
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>Tv</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div class="px-3">
+                    <h5>SHOP</h5>
+                    <ul class="p-0">
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="px-3">
+                <h5>DC</h5>
+                <ul class="p-0">
+                    <li>Terms of Use</li>
+                    <li>Privacy Policy (New)</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <div class="px-3"> 
+                <h5>SITES</h5>
+                <ul class="p-0">
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
+        </div>
+    </footer>
